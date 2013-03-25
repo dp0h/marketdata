aa21aa169a275587851e9349c458a0c4622a92f2
Andrei Visnakovs <andrei.visnakovs@gmail.com>
2013-03-25 01:20:45 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+	name='marketdata', 
+	version='0.1',
+	description='Library for working with Market Data',
+	packages=[
+		'marketdata',
+	],
+	author='Andrei Visnakovs',
+	author_email='andrei@visnakovs.com'
+)
