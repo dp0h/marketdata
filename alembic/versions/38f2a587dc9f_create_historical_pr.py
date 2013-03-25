d0c2c4f3af170ce84251f2752cd88b9dde6ace8e
Andrei Visnakovs <andrei.visnakovs@gmail.com>
2013-03-25 22:59:26 +0000

    Added create history data table script

---
+++ alembic/versions/38f2a587dc9f_create_historical_pr.py
@@ -0,0 +1,22 @@
+"""Create Historical Prices table
+
+Revision ID: 38f2a587dc9f
+Revises: None
+Create Date: 2013-03-25 22:55:40.448294
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '38f2a587dc9f'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
