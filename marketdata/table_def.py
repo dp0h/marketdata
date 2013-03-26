dd95f73f319703f8e5e7ac088b2ff30eb04d4f95
Andrei Visnakovs <andrei.visnakovs@gmail.com>
2013-03-26 00:26:48 +0000

    Fixed issue with primary keys in entities

--- marketdata/table_def.py
+++ marketdata/table_def.py
@@ -12,6 +12,8 @@ Base = declarative_base()
 
 class Symbol(Base):
     __tablename__ = 'symbols'
+
+    id = Column(Integer, primary_key=True)
     name = Column(String)
 
     def __init__(self, name):
@@ -20,6 +22,8 @@ class Symbol(Base):
 
 class HistoricalPrice(Base):
     __tablename__ = 'historical_prices'
+
+    id = Column(Integer, primary_key=True)
     date = Column(DateTime)
     symbol = Column(String)
     open = Column(Numeric(12, 2))
