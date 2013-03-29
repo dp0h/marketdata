f825d5d3b77e65617c532a980abd4bcf2ee22670
Andrei Visnakovs <andrei.visnakovs@gmail.com>
2013-03-29 00:14:36 +0000

    added functions to add, delete and list symbols in db

--- marketdata/table_def.py
+++ marketdata/table_def.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 '''
 Table definitions
 '''
@@ -32,6 +33,16 @@ class HistoricalPrice(Base):
     close = Column(Numeric(12, 2))
     volume = Column(Integer)
     adj_close = Column(Numeric(12, 2))
+
+    def __init__(self, date, symbol, open, high, low, close, volume, adj_close):
+       self.date = date
+       self.symbol = symbol
+       self.open = open
+       self.high = high
+       self.low = low
+       self.close = close
+       self.volume = volume
+       self.adj_close = adj_close
 
 
 def create_all():
--- marketdata/util.py
+++ marketdata/util.py
@@ -7,9 +7,27 @@ from sqlalchemy.orm import sessionmaker
 from table_def import Symbol
 
 
-def add_symbols(symbols):
+def _create_session():
     engine = create_engine('sqlite:///marketdata.db', echo=True)
     Session = sessionmaker(bind=engine)
-    session = Session()
-    session.add_all([Symbol(x) for x in symbols])
+    return Session()
+
+
+def add_symbols(symbols):
+    with _create_session() as session:
+        session.add_all([Symbol(x) for x in symbols])
+        session.commit()
+
+
+def remove_symbols(symbols):
+    session = _create_session()
+    for x in symbols:
+        session.query(Symbol).filter_by(name = x).delete()
+        session.query(HistoricalPrice).filter_by(symbol = x).delete()
     session.commit()
+
+
+def list_symbols():
+    session = _create_session()
+    for s in session.query(Symbol):
+        yield s.Name
