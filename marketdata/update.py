c855e884a1e6d0caf5e4c3c264f790a18fbd9c33
Andrei Visnakovs <andrei.visnakovs@gmail.com>
2013-03-30 01:09:14 +0000

    Implemented cvs values parsing in yahoo.py

--- marketdata/update.py
+++ marketdata/update.py
@@ -2,25 +2,32 @@
 '''
 Market data update functionality
 '''
+from __future__ import print_function
 from sqlalchemy.orm import sessionmaker
-import datetime as dt
+from datetime import datetime, timedelta
 import schema
-from schema import Symbol
+from schema import Symbol, HistoricalPrice
 import yahoo
+
+
+def _parse_date(date):
+    pass
 
 
 def update_marketdata():
     session = sessionmaker(bind=schema.engine).Session()
     symbols = session.query(Symbol)
-    from_date = dt.datetime.now() - dt.timedelta(days=365)  # TODO: change this later
-    to_date = dt.datetime.now() + dt.timedelta(days=2)  # use a future date since there might be issues with timezones
-    for x in symbols:
-        res = yahoo.fetch_market_data(symbol, from_date, to_date)
-        print (res)
-        pass
+    from_date = datetime.now() - timedelta(days=365)  # TODO: change this later
+    to_date = datetime.now() + timedelta(days=2)  # use a future date since there might be issues with timezones
+    for symbol in symbols:
+        (res, data) = yahoo.fetch_market_data(symbol, from_date, to_date)
+        if res:
+            session.add_all([HistoricalPrice(x[0], symbol, x[1], x[2], x[3], x[4], x[5], x[6]) for x in data])
+            session.commit()
+        else:
+            print('Failed updating symbol %s' % symbol)
 
 
     # 2. get latest data from DB
     # 3. fetch required data from yahoo
     # 4. put data to DB
-    pass
--- marketdata/yahoo.py
+++ marketdata/yahoo.py
@@ -4,20 +4,23 @@ Yahoo Market Data API wrappers
 '''
 import urllib
 import urllib2
+from datetime import datetime
 
 
 def fetch_market_data(symbol, from_date, to_date):
+    '''
+    Fetch yahoo market data for a specific symbol.
+    Returns array of data which includes: 'Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close'.
+    '''
     try:
         params = urllib.urlencode({'a': from_date.month - 1, 'b': from_date.day, 'c': from_date.year, 'd': to_date.month - 1, 'e': to_date.day, 'f': to_date.year, 's': symbol})
         url_get = urllib2.urlopen('http://ichart.finance.yahoo.com/table.csv?%s' % params)
-        header = url_get.readline()
+        url_get.readline()  # skip csv header
         data = [url_get.readline()]
         while (len(data[-1]) > 0):
             data.append(url_get.readline())
-        data = [x for x in data if len(x) > 0]
-        #  TODO: parse the date and return as a numpy.array
-        return (True, (header, data))
-    except urllib2.HTTPError:
-        return (False, None)
-    except urllib2.URLError:
-        return (False, None)
+        data = [[x.replace('\n', '') for x in l.split(',')] for l in data if len(l) > 0]
+        data = [(datetime.strptime(x[0], '%Y-%m-%d'), float(x[1]), float(x[2]), float(x[3]), float(x[4]), int(x[5]), float(x[6])) for x in data]
+        return (True, data)
+    except Exception as e:
+        return (False, e)
