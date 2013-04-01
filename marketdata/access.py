138bfe42b3073caed27fc8d2234ae6ee2e92b8ae
Andrei Visnakovs <andrei.visnakovs@gmail.com>
2013-04-01 01:50:53 +0100

    Implemented query in get_marketdata()

--- marketdata/access.py
+++ marketdata/access.py
@@ -2,9 +2,30 @@
 '''
 Market data access functionality
 '''
+from __future__ import print_function
+from sqlalchemy.orm import sessionmaker
+import schema
+from schema import HistoricalPrice
 
 
-def get_marketdata():
-    # 1. fetch specific market data from db
+class Column:
+    Open = HistoricalPrice.open
+    High = HistoricalPrice.high
+    Low = HistoricalPrice.low
+    Close = HistoricalPrice.close
+    Volume = HistoricalPrice.volume
+    AdjClose = HistoricalPrice.adj_close
+
+
+def get_marketdata(symbol, from_date, to_date, columns):
+    '''
+    Retrieve market data for specific symbol
+    '''
+    session = sessionmaker(bind=schema.engine)()
+    query_result = session.query(*([HistoricalPrice.date] + columns)).\
+        filter(HistoricalPrice.symbol == symbol).\
+        filter(HistoricalPrice.date >= from_date).\
+        filter(HistoricalPrice.date <= to_date)
+    for x in query_result:
+        print (x)
     # 2. regurn as pandas stuff
-    pass
