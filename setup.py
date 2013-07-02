1b6b75ffbc7a56b7d3d297ca720ba001b3fbd478
dp0h <andrei.visnakovs@gmail.com>
2013-07-02 01:11:42 +0100

    Fixed typo in requires (setup.py)

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from distutils.core import setup
 
 setup(
-	name = 'marketdata', 
+	name = 'marketdata',
 	version = '0.1',
 	description = 'Library for working with Market Data',
 	packages = ['marketdata'],
-	install_requires = ['numpy', 'pandas', 'sqlachemy', 'pysqlite'],
+	install_requires = ['numpy', 'pandas', 'sqlalchemy', 'pysqlite'],
 	author='Andrei Visnakovs',
 	author_email='andrei@visnakovs.com'
 )
